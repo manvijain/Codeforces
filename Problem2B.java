31c395d5945821c0db6a41b6358789328c0a70b2
manvijain <manvi.jain.india@gmail.com>
2015-01-28 00:48:10 +0530

    28-Jan-2015

--- Problem2B.java
+++ Problem2B.java
@@ -1,8 +1,28 @@
 package codeforce;
 import java.util.*;
-public class CodeForce {
+public class CodeForce
+{
+	static int n;
+	static int[][] board;
+	static int[][] two;
+	static int[][] five;
+	CodeForce()
+	{
+		Scanner in = new Scanner(System.in);
+		n = in.nextInt();
+		board = new int[n][n];
+		two = new int[n][n];
+		five = new int[n][n];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				board[i][j] = in.nextInt();
+			}
+		}
+	}
 
-    public static int find_five(int no)
+	public static int find_five(int no)
     {
     	int c=0;
     	while(no%5==0)
@@ -34,29 +54,131 @@ public class CodeForce {
     		return y;
     }
 
-	public static void main(String args[])
+	public static void board_display()
 	{
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		//initialize a nxn 2-d array
-		int[][] board = new int[n][n];
-		//for processing
-		int[][] process_two = new int[n][n];
-		int[][] process_five = new int[n][n];
-
 		for(int i=0;i<n;i++)
 		{
 			for(int j=0;j<n;j++)
 			{
-				board[i][j] = in.nextInt();
+				System.out.print(board[i][j]+" ");
+			}
+			System.out.println("");
+		}
+	}
+
+	public static void fill_factor_matrices(int row,int column)
+	{
+		if(row==0 && column==0)
+		{
+			two[row][column] = find_two(board[row][column]);
+			five[row][column] = find_five(board[row][column]);
+
+		}
+		else if(row==0 && column!=0)
+		{
+			two[row][column] = two[row][column-1]+find_two(board[row][column]);
+			five[row][column] = five[row][column-1]+find_five(board[row][column]);
+
+		}
+		else if(row!=0 && column==0)
+		{
+			two[row][column] = two[row-1][column]+find_two(board[row][column]);
+			five[row][column] = five[row-1][column]+find_five(board[row][column]);
+
+		}
+		else
+		{
+			int two_up = two[row-1][column]+find_two(board[row][column]);
+			int five_up = five[row-1][column]+find_five(board[row][column]);
+			int  up = return_minimum(two_up,five_up);
+			int two_left = two[row][column-1]+find_two(board[row][column]);
+			int five_left = five[row][column-1]+find_five(board[row][column]);
+			int left = return_minimum(two_left,five_left);
+			if(up<left)
+			{
+				two[row][column] = two_up;
+				five[row][column] = five_up;
+			}
+			else if(up>left)
+			{
+				two[row][column] = two_left;
+				five[row][column] = five_left;
+			}
+			else if(up==left)
+			{
+				if(two_up<two_left)
+				{
+					two[row][column] = two_up;
+				    five[row][column] = five_up;
+				}
+				else if(two_up>two_left)
+				{
+					two[row][column] = two_left;
+				    five[row][column] = five_left;
+				}
+				else if(five_up<five_left)
+				{
+					two[row][column] = two_up;
+				    five[row][column] = five_up;
+				}
+				else if(five_up>=five_left)
+				{
+                    two[row][column] = two_left;
+				    five[row][column] = five_left;
+				}
 			}
 		}
+	}
 
-		int row;
-		int column;
-		int min_two;
-		int min_five;
+	public static String direction_string()
+	{
+		int row = n-1;
+		int column= n-1;
+                int two_calc=0;
+                int five_calc=0;
+		String dir="";
+		while(row!=0 || column!=0)
+		{
+			if(row==0 && column!=0)
+			{
+				dir = "R"+dir;
+				column = column-1;
+			}
+			else if(row!=0 && column==0)
+			{
+				dir="D"+dir;
+				row = row-1;
+			}
+			else
+			{
+				two_calc = two[row][column]-find_two(board[row][column]);
+				five_calc = five[row][column]-find_five(board[row][column]);
+				if(two_calc==two[row-1][column] && five_calc==five[row-1][column])
+				{
+					dir = "D"+dir;
+					row = row-1;
+				}
+				else 
+				{
+					dir = "R"+dir;
+					column = column-1;
+				}
+			}
+		}
+		return dir;
+	} 
 
+	public static int answer()
+	{
+		return return_minimum(five[n-1][n-1],two[n-1][n-1]);
+	}
+
+	public static void main(String args[])
+	{
+		CodeForce obj = new CodeForce();
+		//obj.board_display();
+                int row=0;
+                int column=0;
 		for(int i=0;i<=(n+n-2);i++)
 		{
 			if(i<n)
@@ -65,69 +187,8 @@ public class CodeForce {
 				{
 					row = j;
 					column = i-j;
-					if( j==0 && i==0)
-					{
-						process_two[row][column] = find_two(board[row][column]);
-						process_five[row][column] = find_five(board[row][column]); 
-					}
-					else
-					{
-						if((row-1)>=0 && (column-1)>=0)
-						{
-							if(return_minimum(process_two[row-1][column],process_five[row-1][column])>return_minimum(process_two[row][column-1],process_five[row][column-1]))
-							{
-								min_two = process_two[row][column-1];
-								min_five = process_five[row][column-1];
-								
-							}
-							else if(return_minimum(process_two[row-1][column],process_five[row-1][column])<return_minimum(process_two[row][column-1],process_five[row][column-1]))
-							{
-								min_two = process_two[row-1][column];
-								min_five = process_five[row-1][column];
-								
-							}
-							else
-							{
-								if(process_two[row-1][column]<process_two[row][column-1])
-								{
-									min_two = process_two[row-1][column];
-									min_five = process_five[row-1][column];
+					fill_factor_matrices(row,column);
 
-								}
-								else if(process_two[row-1][column]>process_two[row][column-1])
-								{
-									min_two = process_two[row][column-1];
-									min_five = process_five[row][column-1];
-
-								}
-								else
-								{
-									if(board[row-1][column]<board[row][column-1])
-									{
-										min_two = process_two[row-1][column];
-									    min_five = process_five[row-1][column];
-									}
-									else
-									{
-										min_two = process_two[row][column-1];
-									    min_five = process_five[row][column-1];
-									}
-								}
-							}
-						}
-						else if(row-1<0)
-						{
-							min_two = process_two[row][column-1];
-							min_five = process_five[row][column-1];
-						}
-						else
-						{
-							min_two = process_two[row-1][column];
-							min_five = process_five[row-1][column];
-						}
-						process_two[row][column] =  min_two+find_two(board[row][column]);
-						process_five[row][column] =  min_five+find_five(board[row][column]);
-					}
 				}
 			}
 			else
@@ -136,105 +197,12 @@ public class CodeForce {
 				{
 					row = j;
 					column = i-j;
-
-					if( j==0 && i==0)
-					{
-						process_two[row][column] = find_two(board[row][column]);
-						process_five[row][column] = find_five(board[row][column]); 
-					}
-					else
-					{
-						if((row-1)>=0 && (column-1)>=0)
-						{
-							if(return_minimum(process_two[row-1][column],process_five[row-1][column])>return_minimum(process_two[row][column-1],process_five[row][column-1]))
-							{
-								min_two = process_two[row][column-1];
-								min_five = process_five[row][column-1];
-								
-							}
-							else if(return_minimum(process_two[row-1][column],process_five[row-1][column])<return_minimum(process_two[row][column-1],process_five[row][column-1]))
-							{
-								min_two = process_two[row-1][column];
-								min_five = process_five[row-1][column];
-								
-							}
-							else
-							{
-								if(process_two[row-1][column]<process_two[row][column-1])
-								{
-									min_two = process_two[row-1][column];
-									min_five = process_five[row-1][column];
-
-								}
-								else if(process_two[row-1][column]>process_two[row][column-1])
-								{
-									min_two = process_two[row][column-1];
-									min_five = process_five[row][column-1];
-
-								}
-								else
-								{
-									if(board[row-1][column]<board[row][column-1])
-									{
-										min_two = process_two[row-1][column];
-									    min_five = process_five[row-1][column];
-									}
-									else
-									{
-										min_two = process_two[row][column-1];
-									    min_five = process_five[row][column-1];
-									}
-								}
-							}
-						}
-						else if(row-1<0)
-						{
-							min_two = process_two[row][column-1];
-							min_five = process_five[row][column-1];
-						}
-						else
-						{
-							min_two = process_two[row-1][column];
-							min_five = process_five[row-1][column];
-						}
-						process_two[row][column] =  min_two+find_two(board[row][column]);
-						process_five[row][column] =  min_five+find_five(board[row][column]);
-					}
-				}
-				
+					fill_factor_matrices(row,column);									
+		        }
 		    }
 		}
-
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<n;j++)
-			{
-				System.out.print(process_two[i][j]+" ");
-			}
-			System.out.println("");
-		}
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<n;j++)
-			{
-				System.out.print(process_five[i][j]+" ");
-			}
-			System.out.println("");
-		}
-
-		
+         int answer = obj.answer();
+		System.out.println(answer());
+	    System.out.println(direction_string());
 	}
-
 }
-/*for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<n;j++)
-			{
-				System.out.print(board[i][j]+" ");
-			}
-			System.out.println("");
-		}*/
-
-		//System.out.println(find_five(board[0][0]));
-
-		
